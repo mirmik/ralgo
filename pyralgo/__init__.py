ba51e5c6879872d4f18745ca4d8bde39605247da
mirmik <mirmikns@yandex.ru>
2019-01-12 02:22:12 +0300

    'step'
    Former-commit-id: a9b34f3839cb90c8eb358776e1b44cabbfe0c09f

--- pyralgo/__init__.py
+++ pyralgo/__init__.py
@@ -5,12 +5,41 @@ import numpy as np
 import cmath
 import math
 
-s = sympy.symbols("s")
+import scipy
+import scipy.signal
+
+s, t = sympy.symbols("s t")
 
 class pi(lib.pi):
 	def trfunc(self):
 		return self.kp() + self.ki() * 1/s 
 
+class pid(lib.pi):
+	def __init__(self, kp, ki, kd): self._kp = kp; self._ki = ki; self._kd = kd;  
+	def trfunc(self):
+		return self.kp() + self.ki() * 1/s + self.kd() * s
+	def kp(self): return self._kp
+	def ki(self): return self._ki
+	def kd(self): return self._kd
+	def to_tf(self):
+		return scipy.signal.TransferFunction([self.kd(), self.kp(), self.ki()], [1,0])
+
+
+class pd(lib.pi):
+	def __init__(self, kp, kd): self._kp = kp; self._kd = kd;  
+	def trfunc(self):
+		return self.kp() + self.kd() * s
+	def kp(self): return self._kp
+	def kd(self): return self._kd
+	@staticmethod
+	def by_attrs(K, T): return pd(K, T*K)
+
+class regulator_d(lib.pi):
+	def __init__(self, kd): self._kd = kd;  
+	def trfunc(self):
+		return self.kd() * s
+	def kd(self): return self._kd
+	
 class aperiodic(lib.aperiodic):
 	def trfunc(self):
 		return 1/(self.a()*s+1) 
@@ -18,6 +47,11 @@ class aperiodic(lib.aperiodic):
 class oscilator(lib.oscilator):
 	def trfunc(self):
 		return 1/(self.a()*s*s+self.b()*s+1) 
+
+	@staticmethod	
+	def by_attrs(T, ksi, delta): return oscilator(T**2, 2*ksi*T, delta)
+	#def to_tf(self):
+	#	return scipy.signal.TransferFunction([1], [self.a(), self.b(), 1])
 
 
 def plot_bode(w, start=-5, stop=5, num=100):
@@ -55,5 +89,110 @@ def plot_step_responce(model, time, step):
 		values.append(model(sig))
 	
 	plt.plot(space, values)
+
+def zfunc(w, wo):
+	return (w / (1 + w*wo)).factor()
+
+def numer_denum_poly(w):
+	w=w.factor()
+	numer, denum = sympy.fraction(w)
+	npoly = sympy.Poly(numer, s)
+	dpoly = sympy.Poly(denum, s)
+	return npoly.all_coeffs(), dpoly.all_coeffs()
+
+def roots(w):
+	zf=w.factor()
+	denum = sympy.fraction(zf)[1]
+	poly = sympy.Poly(denum, s)
+	return np.roots(poly.all_coeffs())
+
+def zroots(w):
+	zf=zfunc(w, -1).factor()
+	denum = sympy.fraction(zf)[1]
+	pl = sympy.poly(denum)
+	return np.roots(pl.all_coeffs())
+
+def heaviside(t):
+	def f(t):
+		return 1 if t>=0 else 0
+	return np.vectorize(f)(t)
+
+
+
+def check_dimmension(A,B,C,D) :
+	g_dim = B.shape[-1]
+	x_dim = B.shape[-2]
+	y_dim = C.shape[-2]
+
+	assert(A.shape[-1] == x_dim)
+	assert(A.shape[-2] == x_dim)
+	assert(B.shape[-1] == g_dim)
+	assert(B.shape[-2] == x_dim)
+	assert(C.shape[-1] == x_dim)
+	assert(C.shape[-2] == y_dim)
+	assert(D.shape[-1] == g_dim)
+	assert(D.shape[-2] == y_dim)
+
+	return (g_dim, x_dim, y_dim)
+
+def matrix_discretization(A,B,C,D,Step):
+	_A = scipy.linalg.expm(A * Step)
+	_B = scipy.linalg.inv(A).dot((np.identity(A.shape[0]) - _A).dot(B))
+	_C = C
+	_D = D
+	return (_A,_B,_C,_D)
+
+
+
+
+def plot_step_responce_tf(w, time, points):
+	step = time / points
+	numer, denum = numer_denum_poly(w)
+	numer, denum = np.array(numer), np.array(denum)
+	numer = numer / denum[0]
+	denum = denum / denum[0]
+	dim = len(denum) - 1
+	
+	A = np.zeros((dim, dim))
+	for i in range(0, dim - 1):
+		for j in range(0, dim):
+			A[i,j] = 1 if i + 1 == j else 0
+	for j in range(0, dim):
+		A[dim - 1, j] = -denum[dim - j]
+
+	B = np.zeros((dim, 1))
+	B[dim - 1][0] = -1
+
+	C = np.zeros((1, dim))
+	for j in range(0, min(dim, len(numer))):
+		C[0][j] = numer[j]
+
+	D = np.zeros((1, 1))
+
+	#print(w)
+	#print(numer, denum)
+	#print(A)
+	#print(B)
+	#print(C)
+	#print(D)
+	A,B,C,D = matrix_discretization(A,B,C,D,step)
+
+	#print(A)
+	#print(B)
+	#print(C)
+	#print(D)
+
+	x=np.zeros((dim, 1))
+
+	N = int(time / step)
+
+	arr = []
+	for i in range(0,N):
+		x = A.dot(x) + B
+		arr.append(C.dot(x)[0,0])
+
+	plt.plot(np.r_[0:time:N*1j], arr)
+
+
 
 	
