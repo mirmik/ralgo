419982c404651a8c1c30b13939216b6871fd7330
mirmik <mirmikns@yandex.ru>
2019-01-21 12:36:26 +0300

    'pid'
    Former-commit-id: cf5988f350c6d569a9ecb5d22515a35c423d666a

--- expers/py/test2.py
+++ expers/py/test2.py
@@ -15,7 +15,7 @@ import scipy
 #regulator = ralgo.pd.by_attrs(K=0.001,T=3)
 #regulator = ralgo.pd.by_attrs(K=0.01, T=10)
 regulator = ralgo.pid.by_attrs(K=1, T1=100, T2=1, delta=0.01)
-#model = ralgo.oscilator.by_attrs(T=0.1, ksi=0.1, delta=0.01)
+model = ralgo.oscilator.by_attrs(T=0.1, ksi=0.1, delta=0.01)
 #tf = model.to_tf()
 #st = scipy.signal.step(tf)
 
@@ -29,7 +29,7 @@ regulator = ralgo.pid.by_attrs(K=1, T1=100, T2=1, delta=0.01)
 print(ralgo.zroots(regulator.trfunc()))
 
 #ralgo.plot_step_responce_tf(ralgo.zfunc(w, -1), 20, points = 100)
-ralgo.plot_bode(regulator.trfunc())
+ralgo.plot_bode(regulator.trfunc() * model.trfunc())
 #ralgo.plot_bode(ralgo.zfunc(w, -1))
 
 plt.show()
