e335aef660fed0b66c73f6905a2fddfe2dcff137
mirmik <mirmikns@yandex.ru>
2019-01-22 14:09:28 +0300

    'accdcc traject'
    Former-commit-id: 937f0d3dc70c97a11e02b6b03baabed4baa43810

--- ralgo/planning/traj.h
+++ ralgo/planning/traj.h
@@ -101,6 +101,70 @@ namespace ralgo
 		}
 	};
 
+	template <typename P = int64_t, typename V = float, typename A = float, typename T = time_t>
+	struct accdcc_by_time_trajectory : public trajectory<P, V, A, T>
+	{
+		P x0;
+		P x1;
+		T t_acc;
+		T t_lin;
+		T t_dcc;
+		T t01;
+
+		P xacc;
+		P xlin;
+
+		A acc;
+		A dcc;
+
+		P x01;
+		V v;
+
+		accdcc_by_time_trajectory(P spos, P fpos, T t_acc, T t_lin, T t_dcc) 
+			: x0(spos), x1(fpos), t_acc(t_acc), t_dcc(t_dcc), t_lin(t_lin)
+		{
+			t01 = t_acc + t_lin + t_dcc;
+			x01 = x1 - x0;
+			v = 2 * (V)x01 / (V)(t_acc + 2*t_lin + t_dcc);
+			xacc = t_acc * v / 2;
+			xlin = xacc + t_lin * v;
+
+			A acc = v / t_acc;
+			A dcc = - v / t_dcc;
+		}
+
+		int inloctime(T t, phase<P, V, A>* phs) override
+		{
+			if (t > t01) {
+				phs->d0 = x1;
+				phs->d1 = 0;
+				phs->d2 = 0;
+				return -1;
+			}
+
+			if (t < t_acc) 
+			{
+				phs->d2 = acc;
+				phs->d1 = acc * t;
+				phs->d0 = x0 + phs->d1 * t / 2;
+			}
+			else if (t < t_acc + t_lin) 
+			{
+				phs->d2 = 0;	
+				phs->d1 = v;
+				phs->d0 = xacc + v * t;
+			}
+			else 
+			{
+				phs->d2 = dcc;	
+				phs->d1 = v + dcc * (t - t_acc - t_lin);
+				phs->d0 = xlin + phs->d1 * (t - t_acc - t_lin) / 2;
+			} 
+
+			return 0;
+		}
+	};
+
 }
 
 #endif
