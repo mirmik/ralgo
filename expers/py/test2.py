292f1b48fb01d30423e8b7f6a219cf288ee03d31
mirmik <mirmikns@yandex.ru>
2019-01-23 21:25:44 +0300

    'g'
    Former-commit-id: 8d1b8f62e97f6ade7ab014ab95a0c0c9c6703d5e

--- expers/py/test2.py
+++ expers/py/test2.py
@@ -14,22 +14,23 @@ import scipy
 #regulator = ralgo.regulator_d(0.01)
 #regulator = ralgo.pd.by_attrs(K=0.001,T=3)
 #regulator = ralgo.pd.by_attrs(K=0.01, T=10)
-regulator = ralgo.pid.by_attrs(K=1, T1=100, T2=1, delta=0.01)
-#model = ralgo.oscilator.by_attrs(T=0.1, ksi=0.1, delta=0.01)
+#regulator = ralgo.pid.by_attrs(K=2, T1=1, T2=0.1, delta=0.01)
+model = ralgo.oscilator.by_attrs(T=1, ksi=0.1, delta=0.01)
 #tf = model.to_tf()
 #st = scipy.signal.step(tf)
 
 #print(regulator.trfunc())
 
-#w = model.trfunc() * regulator.trfunc()
+w =  model.trfunc() #model.trfunc() * regulator.trfunc()
 
 #ralgo.plot_bode(model.trfunc())
 #ralgo.plot_bode(regulator.trfunc())
-#print(ralgo.zfunc(w, -1))
-print(ralgo.zroots(regulator.trfunc()))
+print(ralgo.zfunc(w, -1))
+print(ralgo.zroots(w))
 
+ralgo.plot_step_responce_tf(w, 20, points = 100)
 #ralgo.plot_step_responce_tf(ralgo.zfunc(w, -1), 20, points = 100)
-ralgo.plot_bode(regulator.trfunc())
+ralgo.plot_bode(w)
 #ralgo.plot_bode(ralgo.zfunc(w, -1))
 
 plt.show()
