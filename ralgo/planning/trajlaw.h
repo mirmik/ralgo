3b20d0ea5b02def006f05c328162c0896b304e87
mirmik <mirmikns@yandex.ru>
2019-04-30 17:55:28 +0300

    'trajlaw'
    Former-commit-id: 8b59c06886df56035146af9283f42dc2838126ef

--- ralgo/planning/trajlaw.h
+++ ralgo/planning/trajlaw.h
@@ -1,6 +1,79 @@
 #ifndef TRAJLAW_H
 #define TRAJLAW_H
 
+namespace ralgo
+{
+	struct paramlaw_current
+	{
+		float progress; // real trajectory parametr
+		float spdmod; // speed modifier
+	};
 
+	template <class P = int64_t, class V = float, class A = float,
+	          class T = time_t>
+	class paramlaw
+	{
+		virtual int operator()(float t, struct paramlaw_current * out) = 0;
+	};
+
+
+	struct acc_dcc_paramlaw : public paramlaw
+	{
+		float acc;
+		float lin;
+		float dcc;
+
+		float full;
+
+		float pacc;
+		float pdcc;
+		float spd_acceleration_factor;
+
+		acc_dcc_paramlaw(float acc, float lin, float dcc)
+			: acc(acc), lin(lin), dcc(dcc), full(acc + lin + dcc)
+		{
+			spd_acceleration_factor = full / ((acc + dcc) / 2 + lin);
+
+			pacc = acc / full * spd_acceleration_factor;
+			pdcc = dcc / full * spd_acceleration_factor;
+		}
+
+		int operator()(float t, struct paramlaw_current * out)
+		{
+			float p;
+
+			if (t > full)
+			{
+				out->progress = 1;
+				out->spdmod = 0;
+				return -1;
+			}
+
+			else if (t < acc)
+			{
+				p = t / acc;
+				out->progress = p * p * pacc;
+				out->spdmod = p * spd_acceleration_factor;
+			}
+
+			else if (t < lin)
+			{
+				float local = t - acc;
+				p = local / lin;
+
+				out->progress = lerp(pacc, (1 - pdcc), p);
+				out->spdmod = spd_acceleration_factor;
+			}
+
+			else //t< dcc
+			{
+				float local = t - acc - lin;
+				p = 1 - local / dcc;
+				out->progress = (1 - p * p) * pdcc;
+				out->spdmod = p * spd_acceleration_factor;
+			}
+		}
+	}
+}
 
 #endif
