d965477e00deb91680abaf5b3b8e0764341fcb85
mirmik <mirmikns@yandex.ru>
2019-08-13 18:35:25 +0300

    'a'
    Former-commit-id: a0061f442f3a2e308e42523683f7cdac8397c04e

--- ralgo/planning/traj_deformer.h
+++ ralgo/planning/traj_deformer.h
@@ -17,6 +17,9 @@ namespace ralgo
 		float acc;
 		float dcc;
 
+		float a_acc_div_2;
+		float a_dcc_div_2;
+
 		float fini_acc_pos;
 		float strt_dcc_pos;
 
@@ -33,37 +36,60 @@ namespace ralgo
 
 			fini_acc_pos = (1 + strt_spd) / 2 * acc;
 			strt_dcc_pos = 1 - (fini_spd + 1) / 2 * dcc;
+
+			a_acc_div_2 = (1 - strt_spd) / acc / 2;
+			a_dcc_div_2 = (fini_spd - 1) / dcc / 2;
 		}
 
 		float posmod(T t)
 		{
+			/**
+				Расчет траекторного коэффициента идёт по формуле
+				x = x0 + v0*t + a*t*t/2
+				по кусочным участкам.
+			*/
+
 			if (t < acc)
 			{
+				// x0=0 
+				// v0=sspd 
+				// a=(1-sspd)/acc 
 				return
 				    t * strt_spd
-				    + t * t * (1 - strt_spd) / acc / 2;
+				    + t * t * a_acc_div_2;
 			}
 
 			else if (t < fin - dcc)
 			{
+				// x0=facc 
+				// v0=1 
+				// a=0 
 				t = t - acc;  
 				return 
 					fini_acc_pos + 
-					t;// * 1;
+					t;
 			}
 
 			else 
 			{
+				// x0=sdcc 
+				// v0=1 
+				// a=(fspd-1)/dcc 
 				t = t - (fin - dcc);
 				return 
 					strt_dcc_pos 
 					+ t
-					+ t * t * (fini_spd - 1) / dcc / 2; 
+					+ t * t * a_dcc_div_2; 
 			}
 		}
 
 		float spdmod() 
 		{
+			/**
+				Расчет скоростного коэффициента идёт по формуле
+				x = v0 + a*t
+				по кусочным участкам.
+			*/
 
 			if (t < acc)
 			{
