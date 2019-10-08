90135c2486e64481d88f439b12d11f1613d7b421
mirmik <mirmikns@yandex.ru>
2019-10-08 18:28:37 +0300

    'a'
    Former-commit-id: b4356515c4e2754e2e78bead9c9c0fee90dc27ec

--- ralgo/virtdevs/device.h
+++ ralgo/virtdevs/device.h
@@ -5,7 +5,7 @@
 #include <string>
 
 #include <igris/dprint.h>
-#include <igris/event/multiple_delegate.h>
+//#include <igris/event/multiple_delegate.h>
 
 namespace virtdevs 
 {
@@ -18,15 +18,15 @@ namespace virtdevs
 	class device 
 	{
 		Status status = Status::Alarm;		
-		std::string status_string;
+		std::string m_status_string;
 
 		std::vector<device *> supervisors;
 		std::vector<device *> depends;
 		
 		int alarmed_deps = 0;
 
-		igris::multiple_delegate<> alarm_handle; 
-		igris::multiple_delegate<> ready_handle;
+//		igris::multiple_delegate<> alarm_handle; 
+//		igris::multiple_delegate<> ready_handle;
 
 		std::string m_name;
 		
@@ -38,8 +38,8 @@ namespace virtdevs
 		device(const std::string& name) 
 			: m_name(name) 
 		{
-			alarm_handle += igris::make_delegate(&device::alarm_handle_debug, this);
-			ready_handle += igris::make_delegate(&device::ready_handle_debug, this);
+//			alarm_handle += igris::make_delegate(&device::alarm_handle_debug, this);
+//			ready_handle += igris::make_delegate(&device::ready_handle_debug, this);
 		}
 
 		std::string& name() { return m_name; }
@@ -52,19 +52,21 @@ namespace virtdevs
 		void increment_supervisors_alarm_counter();
 		void decrement_supervisors_alarm_counter();
 
-		void alarm_handle_debug() 
+		const std::string& status_string() { return m_status_string; }
+
+		virtual void alarm_handle() 
 		{ 
 			dprln("virtdev::ALARM", name().c_str()); 
 		}
 		
-		void ready_handle_debug() 
+		virtual void ready_handle() 
 		{ 
 			dprln("virtdev::READY", name().c_str()); 
 		}
 
 		void update_alarm_message(const std::string& str) 
 		{
-			status_string = str;
+			m_status_string = str;
 			alarm_handle();
 		}
 	};
