87bf6fcb9c026119b1a72201386619a2dc1bb462
mirmik <mirmikns@yandex.ru>
2019-11-19 18:25:38 +0300

    'a'

--- ralgo/planning/phase_driver.h
+++ ralgo/planning/phase_driver.h
@@ -4,17 +4,23 @@
 #include <ralgo/planning/disctime.h>
 #include <igris/util/iteration_counter.h>
 
+#include <ralgo/virtdevs/device.h>
 #include <nos/trace.h>
 
 namespace ralgo
 {
 	// TODO Шаблонный класс. Задать тип переменной внутреннего состояния.
-	class phase_driver
+	class phase_driver : public virtdevs::device
 	{
 		float poskoeff;
 
 	public:
-		phase_driver()
+		igris::array_view<device*> dependence() override
+		{
+			return {nullptr, 0};
+		}
+
+		phase_driver(const char* drvname) : virtdevs::device(drvname)
 		{
 			set_timeconst(0.01);
 		}
