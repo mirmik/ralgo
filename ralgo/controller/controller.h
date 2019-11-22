e708268157dc8e0a5cb63cda747bc78d27226c41
mirmik <mirmikns@yandex.ru>
2019-11-22 18:49:58 +0300

    'a'
    Former-commit-id: f35f3f088df9ac289ffefa3977f8834e5757bbaf

---
+++ ralgo/controller/controller.h
@@ -0,0 +1,29 @@
+#ifndef RALGO_CONTROLLER_H
+#define RALGO_CONTROLLER_H
+
+#define RALGO_CONTROLLER_FINISH 10
+
+/*namespace ralgo
+{
+	class controller
+	{
+	public:
+		dlist_head controller_executor_lnk;
+		virtual int exec() = 0;
+
+		void serve();
+		void activate();
+		void deactivate();
+	};
+
+	class controller_executor 
+	{
+	public:
+		igris::dlist<controller, &controller::controller_executor_lnk> ctrs;
+
+		void serve();
+		auto size() { return ctrs.size(); }
+	};
+}*/
+
+#endif
---
+++ ralgo/objects/named.h
@@ -0,0 +1,12 @@
+#ifndef RALGO_OBJECTS_NAMED_H
+#define RALGO_OBJECTS_NAMED_H
+
+namespace ralgo 
+{
+	class named 
+	{
+		virtual const char* name() = 0;
+	};
+}
+
+#endif
