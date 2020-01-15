44f129dc87bef96f6481599f86fe7c44397618a7
mirmik <mirmikns@yandex.ru>
2020-01-15 17:39:13 +0300

    'a'
    Former-commit-id: 19876025c7d49249dc3cfa9836ea43d78f8ae1c5

--- expers/heimer/main.cpp
+++ expers/heimer/main.cpp
@@ -13,16 +13,37 @@
 int main() 
 {
 	ralgo::heimer::stepctr_emulator<float, int64_t, float> emul0;	
+	ralgo::heimer::stepctr_emulator<float, int64_t, float> emul1;	
+	emul0.set_name("emul0");
+	emul1.set_name("emul1");
+
+	ralgo::heimer::speed_phaser_axis<float, int64_t, float> ax0(&emul0);
+	ralgo::heimer::speed_phaser_axis<float, int64_t, float> ax1(&emul1);
+	ax0.set_name("ax0");
+	ax1.set_name("ax1");
+
+	ralgo::heimer::device * linint_axes[] = { &ax0, &ax1 };	
+	ralgo::heimer::linear_interpolator<float, float> linint(linint_axes);
+	linint.set_name("linint");
+
+	linint.take_control();
+	linint.print_controlled_devices(nos::current_ostream);
 
 	emul0.set_deltatime(/*ticks_per_second*/10000);
 	emul0.set_gain(4196000);
 	emul0.set_gear(10000);
-	//emul0.set_speed(1);
 
-	ralgo::heimer::speed_phaser_axis<float, int64_t, float> ax0(&emul0);
+	emul1.set_deltatime(/*ticks_per_second*/10000);
+	emul1.set_gain(4196000);
+	emul1.set_gear(10000);
+
 	ax0.set_speed(1);
 	ax0.set_position_compensate(0.01);
-	ax0.incmove(10);
+	//ax0.incmove(10);
+
+	ax1.set_speed(1);
+	ax1.set_position_compensate(0.01);
+	//ax1.incmove(10);
 
 	auto start = std::chrono::system_clock::now();
 	auto waituntil = start;
@@ -32,6 +53,7 @@ int main()
 		while(true) 
 		{
 			emul0.serve();
+			emul1.serve();
 			waituntil = waituntil + std::chrono::microseconds(100);
 			std::this_thread::sleep_until(waituntil);
 		}
@@ -42,6 +64,7 @@ int main()
 		while(true) 
 		{
 			ax0.serve();
+			ax1.serve();
 			std::this_thread::sleep_for(std::chrono::milliseconds(1));
 		}
 	}};
@@ -51,6 +74,7 @@ int main()
 		while(true) 
 		{
 			nos::fprintln("emul0: {} {} {} ax0: {}", emul0.setted_speed(), emul0.feedback_position(), emul0.feedback_position_internal(), ax0.phase());
+			nos::fprintln("emul1: {} {} {} ax0: {}", emul1.setted_speed(), emul1.feedback_position(), emul1.feedback_position_internal(), ax1.phase());
 			std::this_thread::sleep_for(std::chrono::milliseconds(100));
 		}
 	}};
