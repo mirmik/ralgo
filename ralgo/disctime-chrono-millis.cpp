3d1fdc30b944b52f2ae616ad1df551d221b3fd0c
mirmik <mirmikns@yandex.ru>
2021-07-19 15:48:44 +0300

    'a'
    Former-commit-id: 7283490b0fccd7d38351e4a9b49c22f2a99395be

--- ralgo/disctime-chrono-millis.cpp
+++ ralgo/disctime-chrono-millis.cpp
@@ -11,3 +11,14 @@ float ralgo::discrete_time_frequency()
 {
 	return 1000;
 }
+
+int64_t discrete_time()
+{
+	return std::chrono::duration_cast< std::chrono::milliseconds >(
+	           std::chrono::system_clock::now().time_since_epoch()).count();
+}
+
+float discrete_time_frequency()
+{
+	return 1000;
+}
