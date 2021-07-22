cd06d0ca048959e212730dad3c47372acf9d19a8
mirmik <mirmikns@yandex.ru>
2021-07-22 18:18:25 +0300

    'linear_processor'

--- test/axstate_linear_processor.cpp
+++ test/axstate_linear_processor.cpp
@@ -1,2 +1,30 @@
 #include <doctest/doctest.h>
 #include <ralgo/heimer2/axstate_linear_processor.h>
+
+TEST_CASE("axstate_linear_processor") 
+{
+	struct axis_state a, b, c, d;
+	struct axis_state * left[] = { &a, &b };
+	struct axis_state * right[] = { &c, &d };
+
+	struct axstate_linear_processor linproc;
+
+	float matrix[] = { 2, 0.5, 0, 4 }; 
+	float inverse_matrix[4];
+
+	axstate_linear_processor_init(
+		&linproc,
+		"linproc",
+		2,
+		left,
+		right,
+		matrix,
+		inverse_matrix
+	);
+	axstate_linear_processor_evaluate_invertion(&linproc);
+
+	CHECK_EQ(inverse_matrix[0], 0.5);
+	CHECK_EQ(inverse_matrix[1], -0.0625);
+	CHECK_EQ(inverse_matrix[2], 0);
+	CHECK_EQ(inverse_matrix[3], 0.25);
+}
