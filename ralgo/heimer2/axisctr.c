296cdb8c8317bdb71c791ee5a00207ec70b580fc
mirmik <mirmikns@yandex.ru>
2021-07-25 00:14:02 +0300

    'abc'
    Former-commit-id: c3610a89724b12365e269cd691954449aecdf7c3

--- ralgo/heimer2/axisctr.c
+++ ralgo/heimer2/axisctr.c
@@ -2,6 +2,7 @@
 #include <ralgo/heimer2/sigtypes.h>
 #include <igris/math.h>
 #include <igris/util/bug.h>
+#include <igris/datastruct/sparse_array.h>
 
 #include <string.h>
 #include <stddef.h>
@@ -20,15 +21,21 @@ void axis_controller_set_handlers(
 	axctr->operation_finish_handler = operation_finish_handler;
 }
 
-void axis_controller_set_gain(struct axis_controller * axctr, double gain)
+void axis_controller_set_gain(struct axis_controller * axctr, double * gain)
 {
-	axctr->gain = gain;
+	for (int i = 0; i < axctr->dim; ++i)
+	{
+		axctr->settings[i].gain = gain[i];
+	}
 }
 
-void axis_controller_set_limits_internal(struct axis_controller * axctr, position_t back, position_t forw)
+void axis_controller_set_limits_internal(struct axis_controller * axctr, position_t * back, position_t * forw)
 {
-	axctr->backlim = back;
-	axctr->forwlim = forw;
+	for (int i = 0; i < axctr->dim; ++i)
+	{
+		axctr->settings[i].backlim = back[i];
+		axctr->settings[i].forwlim = forw[i];
+	}
 }
 
 void axis_controller_set_velocity_internal(struct axis_controller * axctr, velocity_t speed)
@@ -38,7 +45,7 @@ void axis_controller_set_velocity_internal(struct axis_controller * axctr, veloc
 
 void axis_controller_set_velocity_external(struct axis_controller * axctr, float speed)
 {
-	axctr->vel = speed / discrete_time_frequency() * axctr->gain;
+	axctr->vel = speed / discrete_time_frequency();
 }
 
 void axis_controller_set_accdcc_internal(struct axis_controller * axctr, velocity_t  acc, velocity_t  dcc)
@@ -49,28 +56,33 @@ void axis_controller_set_accdcc_internal(struct axis_controller * axctr, velocit
 
 void axis_controller_set_accdcc_external(struct axis_controller * axctr, float acc, float dcc)
 {
-	axctr->acc = acc / discrete_time_frequency() / discrete_time_frequency() * axctr->gain;
-	axctr->dcc = dcc / discrete_time_frequency() / discrete_time_frequency() * axctr->gain;
+	axctr->acc = acc / discrete_time_frequency() / discrete_time_frequency();
+	axctr->dcc = dcc / discrete_time_frequency() / discrete_time_frequency();
 }
 
-void axis_controller_set_limits_external(struct axis_controller * axctr, double back, double forw)
+void axis_controller_set_limits_external(struct axis_controller * axctr, double * back, double * forw)
 {
-	axctr->backlim = back * axctr->gain;
-	axctr->forwlim = forw * axctr->gain;
+	for (int i = 0; i < axctr->dim; ++i)
+	{
+		axctr->settings[i].backlim = back[i] * axctr->settings[i].gain;
+		axctr->settings[i].forwlim = forw[i] * axctr->settings[i].gain;
+	}
 }
 
 void axis_controller_set_controlled(struct axis_controller * axctr, struct axis_state * state)
 {
-	axctr->controlled = state;
-
-	signal_head_get(&axctr->controlled->sig);
+	for (int i = 0; i < axctr->dim; ++i)
+	{
+		axctr->settings[i].controlled = &state[i];
+		signal_head_get(&axctr->settings[i].controlled->sig);
+	}
 }
 
-void axis_controller_finish_trajectory(struct axis_controller * axctr, disctime_t time, position_t ctrpos)
+void axis_controller_finish_trajectory(struct axis_controller * axctr, disctime_t time, position_t * ctrpos)
 {
 	axctr->operation_finished_flag = 1;
 	axctr->operation_finish_handler(axctr->operation_handlers_priv, axctr);
-	line_trajectory_set_point_hold(&axctr->lintraj, time, &ctrpos);
+	line_trajectory_set_point_hold(&axctr->lintraj, time, ctrpos);
 	axctr->curtraj = &axctr->lintraj.traj;
 }
 
@@ -82,21 +94,28 @@ void axis_controller_feedback(struct signal_processor * sigproc, disctime_t time
 void axis_controller_serve(struct signal_processor * sigproc, disctime_t time)
 {
 	struct axis_controller * axctr =  mcast_out(sigproc, struct axis_controller, sigproc);
-	position_t ctrpos;
-	velocity_t ctrvel;
+	position_t ctrpos[axctr->dim];
+	velocity_t ctrvel[axctr->dim];
 
-	if (axctr->controlled->sig.current_controller && axctr->controlled->sig.current_controller != sigproc) 
-		return;
+	for (int i = 0; i < axctr->dim; ++i)
+	{
+		if (
+		    axctr->settings[i].controlled->sig.current_controller &&
+		    axctr->settings[i].controlled->sig.current_controller != sigproc
+		)
+			return;
+	}
 
 	if (!axctr->curtraj)
 		return;
 
-	// Установить текущие целевые параметры.
-	int sts = axctr->curtraj->attime(axctr->curtraj, time, &ctrpos, &ctrvel);
-	axctr->controlled->ctrpos = ctrpos;
-	axctr->controlled->ctrvel = ctrvel;
-
-	assert(!isnan(ctrvel));
+	int sts = axctr->curtraj->attime(axctr->curtraj, time, ctrpos, ctrvel);
+	for (int i = 0; i < axctr->dim; ++i)
+	{
+		// Установить текущие целевые параметры.
+		axctr->settings[i].controlled->ctrpos = ctrpos[i];
+		axctr->settings[i].controlled->ctrvel = ctrvel[i];
+	}
 
 	if (sts && !axctr->operation_finished_flag)
 	{
@@ -108,17 +127,18 @@ static
 int __axis_controller_absmove(
     struct axis_controller * axctr,
     disctime_t curtim,
-    position_t curpos,
-    position_t tgtpos)
+    position_t * curpos,
+    position_t * tgtpos)
 {
-	if (axctr->controlled->sig.current_controller) 
-	{
-		return -1;
-	}
+	for (int i = 0; i < axctr->dim; ++i)
+		if (axctr->settings[i].controlled->sig.current_controller)
+		{
+			return -1;
+		}
 
 	if (signal_processor_activate(&axctr->sigproc))
 		return -1;
-	
+
 	position_t dist = tgtpos - curpos;
 	disctime_t tgttim = curtim + (float)(ABS(dist)) / axctr->vel;
 
@@ -134,8 +154,8 @@ int __axis_controller_absmove(
 	line_trajectory_init_nominal_speed(&axctr->lintraj,
 	                                   curtim,
 	                                   tgttim,
-	                                   &curpos,
-	                                   &tgtpos,
+	                                   curpos,
+	                                   tgtpos,
 	                                   acc_time,
 	                                   dcc_time,
 	                                   axctr->spattern_enabled
@@ -149,33 +169,48 @@ int __axis_controller_absmove(
 }
 
 
-int axis_controller_incmove(struct axis_controller * axctr, disctime_t current_time, double dist_real)
+int axis_controller_incmove(struct axis_controller * axctr, disctime_t current_time, double * dist_real)
 {
-	position_t dist = dist_real * axctr->gain;
+	position_t curpos[axctr->dim];
+	position_t tgtpos[axctr->dim];
 
-	position_t curpos = axctr->controlled->ctrpos;
-	position_t tgtpos = curpos + dist;
+	for (int i = 0; i < axctr->dim; ++i)
+	{
+		position_t dist = dist_real[i] * axctr->settings[i].gain;
 
-	tgtpos = CLAMP(tgtpos,
-	               axctr->backlim,
-	               axctr->forwlim);
+		curpos[i] = axctr->settings[i].controlled->ctrpos;
+		tgtpos[i] = curpos[i] + dist;
+
+		tgtpos[i] = CLAMP(tgtpos[i],
+		                  axctr->settings[i].backlim,
+		                  axctr->settings[i].forwlim);
+	}
 
 	return __axis_controller_absmove(axctr, current_time, curpos, tgtpos);
 }
 
-int axis_controller_absmove(struct axis_controller * axctr, disctime_t current_time, double pos_real)
+int axis_controller_absmove(struct axis_controller * axctr, disctime_t current_time, double * pos_real)
 {
-	position_t curpos = axctr->controlled->ctrpos;
-	position_t tgtpos = tgtpos * axctr->gain;
 
-	tgtpos = CLAMP(tgtpos,
-	               axctr->backlim,
-	               axctr->forwlim);
+	position_t curpos[axctr->dim];
+	position_t tgtpos[axctr->dim];
+
+	for (int i = 0; i < axctr->dim; ++i)
+	{
+		position_t dist = pos_real[i] * axctr->settings[i].gain;
+
+		curpos[i] = axctr->settings[i].controlled->ctrpos;
+		tgtpos[i] = tgtpos[i] * axctr->settings[i].gain;
+
+		tgtpos[i] = CLAMP(tgtpos[i],
+		                  axctr->settings[i].backlim,
+		                  axctr->settings[i].forwlim);
+	}
 
 	return __axis_controller_absmove(axctr, current_time, curpos, tgtpos);
 }
 
-float axis_controller_ctrpos_external(struct axis_controller * axctr)
+/*float axis_controller_ctrpos_external(struct axis_controller * axctr)
 {
 	return  axctr->controlled->ctrpos / axctr->gain;
 }
@@ -188,23 +223,24 @@ float axis_controller_feedpos_external(struct axis_controller * axctr)
 float axis_controller_ctrvel_external(struct axis_controller * axctr)
 {
 	return axctr->controlled->ctrvel * discrete_time_frequency() / axctr->gain;
-}
+}*/
 
-struct axis_controller * create_axis_controller(const char * name)
+struct axis_controller * create_axis_controller(const char * name, int dim)
 {
 	struct axis_controller * ptr = (struct axis_controller *) malloc(sizeof(struct axis_controller));
-	axis_controller_init(ptr, name);
+	struct axis_settings * settings = (struct axis_settings *) malloc(sizeof(struct axis_settings) * dim);
+	axis_controller_init(ptr, name, settings, dim);
 	return ptr;
 }
 
-void axis_controller_release_controlled(struct axis_controller * axctr)
+/*void axis_controller_release_controlled(struct axis_controller * axctr)
 {
 	if (axctr->controlled)
 	{
 		signal_head_put(&axctr->controlled->sig);
 		axctr ->controlled = NULL;
 	}
-}
+}*/
 
 void axis_controller_deinit(struct signal_processor * sigproc)
 {
@@ -217,10 +253,17 @@ struct signal_head * axis_controller_iterate_left(struct signal_processor * sigp
 	struct axis_controller * axctr = mcast_out(sigproc, struct axis_controller, sigproc);
 
 	if (iter == NULL)
-		return &axctr->controlled->sig;
+		return &axctr->settings[0].controlled->sig;
 
-	else
-		return NULL;
+	for (int i = 0; i < axctr->dim - 1; ++i) 
+	{
+		if (iter == &axctr->settings[i].controlled->sig) 
+		{
+			return &axctr->settings[i+1].controlled->sig;
+		}
+	}
+
+	return NULL;
 }
 
 const struct signal_processor_operations axisctr_ops =
@@ -232,7 +275,12 @@ const struct signal_processor_operations axisctr_ops =
 	.iterate_left = axis_controller_iterate_left
 };
 
-void axis_controller_init(struct axis_controller * axctr, const char * name)
+void axis_controller_init(
+    struct axis_controller * axctr,
+    const char * name,
+    struct axis_settings * setings,
+    int dim
+)
 {
 	signal_processor_init(&axctr->sigproc, name, &axisctr_ops);
 
@@ -240,10 +288,10 @@ void axis_controller_init(struct axis_controller * axctr, const char * name)
 	axctr->acc = 0;
 	axctr->dcc = 0;
 
-	axctr->gain = 0;
+	//axctr->gain = 0;
 
-	axctr->backlim = 0;
-	axctr->forwlim = 0;
+	//axctr->backlim = 0;
+	//axctr->forwlim = 0;
 
 	axctr->operation_finished_flag = 0;
 
@@ -253,10 +301,10 @@ void axis_controller_init(struct axis_controller * axctr, const char * name)
 
 	axctr->spattern_enabled = 0;
 	axctr->curtraj = NULL;
-	axctr->controlled = NULL;
+	//axctr->controlled = NULL;
 
 	line_trajectory_init(&axctr->lintraj, 1,
-	                     &axctr->_line_trajectory_sfpos,
-	                     0
+	                     &axctr->settings[0].sfpos,
+	                     dim
 	                    );
 }
