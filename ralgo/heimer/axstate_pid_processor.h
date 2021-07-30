b091a1376f3fbce6547b1f41050b6d7ce3930ad4
mirmik <mirmikns@yandex.ru>
2021-07-30 18:49:33 +0300

    'a'
    Former-commit-id: 47cc33755716f389f3cf157bb31407e83a87cafc

---
+++ ralgo/heimer/axstate_pid_processor.h
@@ -0,0 +1,23 @@
+#ifndef RALGO_HEIMER_AXSTATE_PID_PROCESSOR_H
+#define RALGO_HEIMER_AXSTATE_PID_PROCESSOR_H
+
+#include <ralgo/heimer/axis_state.h> 
+
+namespace heimer 
+{
+	class axstate_pid_processor 
+	{
+		axis_state * left;
+		axis_state * right;
+
+	public:
+		int feedback(disctime_t time) override;
+		int serve(disctime_t time) override;
+		int command(int argc, char ** argv, char * output, int outmax) override;
+		void deinit() override;
+		signal_head * iterate_left(signal_head *) override;
+		signal_head * iterate_right(signal_head *) override;	
+	}
+}
+
+#endif
