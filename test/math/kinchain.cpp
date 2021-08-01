1310ed6bf85187cb4fe1e17b0fb4c458d45ce78b
mirmik <mirmikns@yandex.ru>
2021-08-02 00:41:26 +0300

    'a'

--- test/math/kinchain.cpp
+++ test/math/kinchain.cpp
@@ -87,3 +87,60 @@ TEST_CASE("kinchain.4")
 	CHECK_EQ(outsenses[0].lin[2], doctest::Approx(0));
 	CHECK_EQ(outsenses[0].ang, linalg::vec<double,3>{0,0,1});
 }
+
+TEST_CASE("kinchain.5")
+{
+	rabbit::pose3<double> constants[2] = { rabbit::rot3<double>({0,0,1},M_PI/2), rabbit::mov3<double>({1,0,0}) };
+	rabbit::screw3<double> locsenses[1] = { {{0,0,1}, {0,0,0}} };
+	double coords[1] = { M_PI/2 };
+	rabbit::screw3<double> outsenses[1];
+
+	ralgo::kinematic_chain_sensivities(
+	    constants,
+	    locsenses,
+	    coords,
+	    1,
+	    outsenses
+	);
+
+	CHECK_EQ(outsenses[0].lin[0], doctest::Approx(0));
+	CHECK_EQ(outsenses[0].lin[1], doctest::Approx(-1));
+	CHECK_EQ(outsenses[0].lin[2], doctest::Approx(0));
+	CHECK_EQ(outsenses[0].ang, linalg::vec<double,3>{0,0,1});
+}
+
+TEST_CASE("kinchain.6")
+{
+	rabbit::pose3<double> constants[3] = { 
+		rabbit::mov3<double>({1,0,0}), 
+		rabbit::mov3<double>({1,0,0}), 
+		rabbit::mov3<double>({1,0,0}) 
+	};
+	
+	rabbit::screw3<double> locsenses[2] = { {{0,0,1}, {0,0,0}}, {{0,0,1}, {0,0,0}} };
+	
+	double coords[2] = { M_PI/4, M_PI/4 };
+	rabbit::screw3<double> outsenses[2];
+
+	ralgo::kinematic_chain_sensivities(
+	    constants,
+	    locsenses,
+	    coords,
+	    2,
+	    outsenses
+	);
+
+	CHECK_EQ(outsenses[0].lin[0], doctest::Approx(-(1+1/sqrt(2))));
+	CHECK_EQ(outsenses[0].lin[1], doctest::Approx(1/sqrt(2)));
+	CHECK_EQ(outsenses[0].lin[2], doctest::Approx(0));
+	CHECK_EQ(outsenses[0].ang[0], doctest::Approx(0));
+	CHECK_EQ(outsenses[0].ang[1], doctest::Approx(0));
+	CHECK_EQ(outsenses[0].ang[2], doctest::Approx(1));
+
+	CHECK_EQ(outsenses[1].lin[0], doctest::Approx(-1));
+	CHECK_EQ(outsenses[1].lin[1], doctest::Approx(0));
+	CHECK_EQ(outsenses[1].lin[2], doctest::Approx(0));
+	CHECK_EQ(outsenses[1].ang[0], doctest::Approx(0));
+	CHECK_EQ(outsenses[1].ang[1], doctest::Approx(0));
+	CHECK_EQ(outsenses[1].ang[2], doctest::Approx(1));
+}
