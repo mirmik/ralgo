f5ab27c1452c43d4f56f2a62cbe7000c7a0399a1
mirmik <mirmikns@yandex.ru>
2021-08-02 17:40:18 +0300

    'a'

---
+++ ralgo/heimer/axstate_pose3_chain_processor.cpp
@@ -0,0 +1,62 @@
+#include <ralgo/heimer/axstate_pose3_chain_processor.h>
+
+heimer::axstate_pose3_chain_processor::axstate_pose3_chain_processor(const char * name, int leftdim) 
+	: signal_processor(name)
+{
+
+}
+
+rabbit::pose3<position_t> heimer::axstate_pose3_chain_processor::evaluate_target_position()
+{
+	return {};
+}
+
+rabbit::screw3<position_t> heimer::axstate_pose3_chain_processor::evaluate_target_velocity()
+{
+	return {};
+}
+
+rabbit::screw3<position_t> heimer::axstate_pose3_chain_processor::evaluate_position_error()
+{
+	return {};
+}
+
+int heimer::axstate_pose3_chain_processor::feedback(disctime_t time)
+{
+	return 0;
+}
+
+int heimer::axstate_pose3_chain_processor::serve(disctime_t time)
+{
+	return 0;
+}
+
+int heimer::axstate_pose3_chain_processor::command(int argc, char ** argv, char * output, int outmax)
+{
+	return 0;
+}
+
+void heimer::axstate_pose3_chain_processor::deinit()
+{
+
+}
+
+void heimer::axstate_pose3_chain_processor::on_activate(disctime_t)
+{
+
+}
+
+void heimer::axstate_pose3_chain_processor::evaluate_error()
+{
+
+}
+
+void heimer::axstate_pose3_chain_processor::evaluate_output_sensivities(rabbit::screw3<double> * sensivities)
+{
+
+}
+
+void heimer::axstate_pose3_chain_processor::backpack(rabbit::screw3<double> * sensivities)
+{
+
+}
