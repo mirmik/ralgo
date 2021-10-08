ec43f5e10dd4fe421eeb3aea69dd2815916dde79
mirmik <mirmikns@yandex.ru>
2021-10-08 13:58:47 +0300

    'a'

--- ralgo/heimer/fast_cycle_device.cpp
+++ ralgo/heimer/fast_cycle_device.cpp
@@ -1,3 +1,6 @@
 #include <ralgo/heimer/fast_cycle_device.h>
+#include <igris/util/bug.h>
 
 DLIST_HEAD(heimer::fast_cycle_list);
+
+__attribute__((weak)) double heimer::fast_cycle_frequence() { BUG(); return 0; }
