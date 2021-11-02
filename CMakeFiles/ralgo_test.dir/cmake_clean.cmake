894e34587116410f6c197b21788ebbc7937c7bbf
mirmik <mirmikns@yandex.ru>
2021-11-02 18:42:39 +0300

    'cmake'
    Former-commit-id: b0bceb9605caacb04cb929848e79af1f50117024

---
+++ CMakeFiles/ralgo_test.dir/cmake_clean.cmake
@@ -0,0 +1,18 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ralgo_test.dir/test/clinalg.cpp.o"
+  "CMakeFiles/ralgo_test.dir/test/index_brute_force.cpp.o"
+  "CMakeFiles/ralgo_test.dir/test/linetraj.cpp.o"
+  "CMakeFiles/ralgo_test.dir/test/magnetometer.cpp.o"
+  "CMakeFiles/ralgo_test.dir/test/main.cpp.o"
+  "CMakeFiles/ralgo_test.dir/test/point_in_hexagon.cpp.o"
+  "CMakeFiles/ralgo_test.dir/test/quadgen.cpp.o"
+  "CMakeFiles/ralgo_test.dir/test/sliding_array.cpp.o"
+  "CMakeFiles/ralgo_test.dir/test/tsdeform.cpp.o"
+  "ralgo_test"
+  "ralgo_test.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ralgo_test.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
--- ralgo/HIDE/heimer/signal.cpp
+++
@@ -1,9 +0,0 @@
-/** @file */
-
-#include <ralgo/heimer/signal.h>
-#include <igris/dprint.h>
-
-void heimer::send_signal(int signal, void * source) 
-{
-	dprln("WARNING: %d", signal);
-}
