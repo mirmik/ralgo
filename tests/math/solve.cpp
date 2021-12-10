b12912309903e0b28acf9cdae4d51caa062eda02
mirmik <mirmikns@yandex.ru>
2021-12-10 16:07:21 +0300

    'linalg'
    Former-commit-id: 205e4985bfdde4992fa4d6f06ac1e0a40d62ec35

--- tests/math/solve.cpp
+++ tests/math/solve.cpp
@@ -58,17 +58,17 @@ TEST_CASE("U_triangle_solve")
 TEST_CASE("solve")
 {
     ralgo::matrix<float> A;
-    std::vector<float> x, b;
+    ralgo::vector<float> x, b;
 
     A = ralgo::matops::diag({1.f, 1.f, 1.f});
-    x = std::vector{1.f, 1.f, 1.f};
+    x = ralgo::vector{1.f, 1.f, 1.f};
     b = ralgo::solve(A, x);
     CHECK_EQ(b[0], 1);
     CHECK_EQ(b[1], 1);
     CHECK_EQ(b[2], 1);
 
     A = ralgo::matops::diag({2.f, 1.f, 2.f});
-    x = std::vector{1.f, 1.f, 1.f};
+    x = ralgo::vector{1.f, 1.f, 1.f};
     b = ralgo::solve(A, x);
     CHECK_EQ(b[0], 0.5);
     CHECK_EQ(b[1], 1);
