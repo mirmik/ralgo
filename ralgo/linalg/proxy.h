d88839e2dfc7b796613331414f2d34714e784f5a
mirmik <mirmikns@yandex.ru>
2022-04-09 19:35:27 +0300

    'simplex and decomposition comments'
    Former-commit-id: 38c9afe7ecc3b8d6299f973647e8ce35dd40fc9d

--- ralgo/linalg/proxy.h
+++ ralgo/linalg/proxy.h
@@ -1,6 +1,13 @@
 #ifndef RALGO_LINALG_TRANSPOSED_MATRIX_VIEW_H
 #define RALGO_LINALG_TRANSPOSED_MATRIX_VIEW_H
 
+#include <vector>
+
+/** 
+ *  Матрицы-прокси. Оболочки, имеющие интерфейс матрицы, но обращающиеся за данными
+ *  к внешним источникам, или совершающие над данными предварительные операции.  
+ **/
+
 namespace ralgo
 {
     template <class M> class transposed_matrix_proxy
@@ -35,6 +42,33 @@ namespace ralgo
         int cols() const { return vec.size(); }
     };
 
+    template <class V> class columns_proxy_matrix 
+    {
+        std::vector<std::reference_wrapper<V>> vecs;
+        
+    public:
+        columns_proxy_matrix(std::vector<std::reference_wrapper<V>> &vecs) 
+            : vecs(vecs)
+        {}
+
+        auto at(int i, int j) const { return vecs[j][i]; }
+        int rows() const { return vecs[0].size(); }
+        int cols() const { return vecs.size(); }
+    };
+
+    template <class V> class rows_proxy_matrix 
+    {
+        std::vector<std::reference_wrapper<V>> vecs;
+        
+    public:
+        rows_proxy_matrix(std::vector<std::reference_wrapper<V>> &vecs) 
+            : vecs(vecs)
+        {}
+
+        auto at(int i, int j) const { return vecs[i][j]; }
+        int rows() const { return vecs.size(); }
+        int cols() const { return vecs[0].size(); }
+    };
 }
 
 #endif
