25bd817a9ad8736d724156f09cf746253a8005e7
mirmik <mirmik@yandex.ru>
2022-05-26 18:30:46 +0300

    geom

--- tests/geom.cpp
+++ tests/geom.cpp
@@ -13,3 +13,18 @@ TEST_CASE("scalar_bivector")
     CHECK_EQ(geommul(b, c), ralgo::geom::scalar_bivector<double>{3, {0, 4, 0}});
     CHECK_EQ(geommul(d, f), ralgo::geom::scalar_bivector<double>{0, {0, 0, -1}});
 }
+
+
+TEST_CASE("scalar_bivector.rotor") 
+{
+    auto rotor = ralgo::geom::rotor<double>({0, 0, M_PI});
+    CHECK_EQ(rotor.e, doctest::Approx(0));
+    CHECK_EQ(rotor.e23, doctest::Approx(0));
+    CHECK_EQ(rotor.e31, doctest::Approx(0));
+    CHECK_EQ(rotor.e12, doctest::Approx(1));    
+
+    auto unrotor = ralgo::geom::unrotor<double>(rotor);
+    CHECK_EQ(unrotor.e23, doctest::Approx(0));
+    CHECK_EQ(unrotor.e31, doctest::Approx(0));
+    CHECK_EQ(unrotor.e12, doctest::Approx(M_PI));    
+} 
