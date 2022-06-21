f6c7206c81a2ebad4cec024379de9aa2a9c94326
mirmik <mirmikns@yandex.ru>
2022-06-22 00:08:37 +0300

    'a'
    Former-commit-id: 53651f04bab44297975090901ea03bee1155804e

--- tests/tensor.cpp
+++ tests/tensor.cpp
@@ -13,11 +13,11 @@ TEST_CASE("antisymmetric_tensor")
 TEST_CASE("steiner_theorem") 
 {
     double mass = 1;
-    double3 local_center = {1,0,0};
+    double3 local_center = {0,0,1};
     double3 local_inertia = {1,1,1};
-    ralgo::pose3<double> world_pose = {{}, {}};
+    ralgo::pose3<double> world_pose = {{0,0,0,1}, {0,0,0}};
     double3 target_center = {0,0,0};
 
     auto res = ralgo::steiner(mass, local_inertia, local_center, world_pose, target_center);
-    CHECK(res == linalg::mat<double, 3, 3>({{1, 0, 0}, {0, 1, 0}, {0, 0, 1}}));
+    CHECK(res == linalg::mat<double, 3, 3>({{2, 0, 0}, {0, 2, 0}, {0, 0, 1}}));
 }
