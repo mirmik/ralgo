42b5f67de6cb5524a70530978c26006f978e078c
mirmik <mirmikns@yandex.ru>
2022-11-25 21:18:21 +0300

    'remove cnc math test'

--- tests/cncmath.cpp
+++
@@ -1,9 +0,0 @@
-#include <doctest/doctest.h>
-#include <ralgo/cnc/math.h>
-
-TEST_CASE("cnc.math") 
-{
-	ralgo::vector<double> arg{0,11,0};
-	ralgo::vector<double> dir = ralgo::vecops::normalize<ralgo::vector<double>>(arg);
-	CHECK_EQ(dir, ralgo::vector<double>{0,1,0});
-}
