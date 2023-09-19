ede39a19c4ab3aeef57628bbba32cb3af73fb1ce
mirmik <mirmikns@yandex.ru>
2023-09-19 18:48:03 +0300

    'line trajectory'

--- ralgo/trajectory/LineTrajectory.h
+++ ralgo/trajectory/LineTrajectory.h
@@ -5,12 +5,14 @@ namespace ralgo
 {
     template <class Time, class Position> class LinePositionFunction
     {
-        Time start_time;
-        Time finish_time;
-        Position start_position;
-        Position finish_position;
+        Time start_time = 0;
+        Time finish_time = 1;
+        Position start_position = 0;
+        Position finish_position = 0;
 
     public:
+        LinePositionFunction() = default;
+
         LinePositionFunction(Time start_time,
                              Time finish_time,
                              Position start_position,
@@ -22,11 +24,21 @@ namespace ralgo
 
         Position operator()(Time time)
         {
-            auto k = (time - start_time) / (finish_time - start_time);
+            if (time >= finish_time)
+                return finish_position;
+            if (time <= start_time)
+                return start_position;
+            auto k = (double)(time - start_time) /
+                     (double)(finish_time - start_time);
             auto result = start_position * (1 - k) + finish_position * (k);
             return result;
         }
+
+        bool in_interval(Time time)
+        {
+            return time >= start_time && time <= finish_time;
+        }
     };
-}
+} // namespace ralgo
 
 #endif
