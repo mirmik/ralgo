3bd958372189b22e6e80d24ef1bdeaf58a3196e1
nfsorokin <sorokin@smitek-robotics.com>
2025-12-03 14:41:41 +0300

    vasdvasdv

---
+++ tools/subtrees-pull.sh
@@ -0,0 +1,5 @@
+git subtree pull --prefix=igris igris-origin master --squash
+git subtree pull --prefix=nos nos-origin master --squash
+git subtree pull --prefix=crow crow-origin master --squash
+git subtree pull --prefix=genos genos-origin master --squash
+git subtree pull --prefix=ralgo ralgo-origin master --squash
